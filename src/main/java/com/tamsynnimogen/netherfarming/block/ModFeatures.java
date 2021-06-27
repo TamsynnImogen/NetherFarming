a74b0148e0d155125f6f79b23cb21b759a9fb854
TamsynnImogen <apocalypse_unleashed@hotmail.co.uk>
2021-06-27 23:40:18 +0100

    1.16.5-36.1.24-1.2a: - Vegitation generation testings - Bloodbark fungus, glowberry bushes and soul roots spawn if fertile soul soil is bonemealed - Huge bloodbark fungus naturally generate on soul soil - Nether seeds now obtainable from soul roots instead from soul fire - Using a flint & steel on fertile soul soil reverts it back to regular soul soil - Missing recipes for hellkin and gloom squash seeds added

--- src/main/java/com/tamsynnimogen/netherfarming/block/ModFeatures.java
+++ src/main/java/com/tamsynnimogen/netherfarming/block/ModFeatures.java
@@ -9,9 +9,6 @@ import net.minecraft.world.gen.feature.*;
 
 public class ModFeatures {
 
-
-
-
     public static final class Configs {
         public static final BlockStateProvidingFeatureConfig SOUL_SOIL_VEGETATION_CONFIG = new BlockStateProvidingFeatureConfig((new WeightedBlockStateProvider()).addWeightedBlockstate(ModFeatures.States.SOUL_ROOTS, 85).addWeightedBlockstate(ModFeatures.States.BLOODBARK_FUNGUS, 10).addWeightedBlockstate(ModFeatures.States.GLOWBERRY_CROP, 5));
     }
